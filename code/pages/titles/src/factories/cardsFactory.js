1ed748c57f4d9dca826c53b00c1a704d3e2e6acb
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-27 13:46:08 -0300

    updating cards

--- code/pages/titles/src/factories/cardsFactory.js
+++ code/pages/titles/src/factories/cardsFactory.js
@@ -7,9 +7,8 @@ const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
     return CardsController.initialize({
-      worker: cardListWorker,
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json`, cardListWorker })
     })
   }
 }
